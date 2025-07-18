5752411f4f6cd063a265caa1b397aea1ed141215
jrKitt <kittichdev@gmail.com>
2025-07-19 00:48:20 +0700

    feat: circlequeue

--- Lab03/src/CircleQueue.java
+++ Lab03/src/CircleQueue.java
@@ -4,57 +4,49 @@
 public class CircleQueue {
     private int front = -1;
     private int back = -1;
-    private int[] queue;
-    private int size = 0;
-    private int c = 0;
+    private int[] q;
 
-    CircleQueue(int arr) {
-        this.size = arr;
-        this.queue = new int[arr];
+    CircleQueue(int size) {
+        this.q = new int[size];
     }
 
     CircleQueue() {
-        this.size = 5;
-        this.queue = new int[5];
+        this.q = new int[5];
     }
 
     public void enqueue(int arr) {
-        if (c == size) {
+        if ((back + 1) % q.length == front) {
             System.out.println("\n...Fail...");
             return;
         }
         if (front == -1) {
             front = 0;
         }
-        back = (back + 1) % size;
-        queue[back] = arr;
-        c++;
-
+        back = (back + 1) % q.length;
+        q[back] = arr;
         System.out.println("\n...Success...");
     }
 
     public int dequeue() {
-        if (c == 0) {
+        if (front == -1) {
             System.out.println("\n...Fail...");
             return -1;
         }
-        int arr = queue[front];
-
-        if (c == 1) {
+        int arr = q[front];
+        if (front == back) {
             front = -1;
             back = -1;
         } else {
-            front = (front + 1) % size;
+            front = (front + 1) % q.length;
         }
-        c--;
         return arr;
     }
 
     public void printQueue() {
-        if (c == 0 || front == -1) {
+        if (front == -1) {
             System.out.println("No data");
             return;
         }
-        System.out.println("\nData in Queue : " + queue[back]);
+        System.out.println("\nData in Queue : " + q[back]);
     }
 }
